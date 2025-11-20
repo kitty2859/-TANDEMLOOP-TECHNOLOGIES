d21ca591e34afae1e4935520a76116713897a4a9
kitty2859 <79400597+kitty2859@users.noreply.github.com>
2025-11-20 14:33:19 +0530

    Update Problem1.java

--- Problem1.java
+++ Problem1.java
@@ -1,40 +1,44 @@
 import java.util.Scanner;
 
-class Problem1{
+class Calculator {
     double a;
     double b;
     String opr;
 
-    
-     double calculate() {
+    double calculate() {
         if (opr.equals("Add")) {
-        return a + b;
-    } else if (opr.equals("Subtract")) {
-        return a - b;
-    } else if (opr.equals("Multiply")) {
-        return a * b;
-    } else if (opr.equals("Divide")) {
-        return a / b;
-    } else {
-      System.out.print("Invalid operations  : ");
-      return 0;  
-       }
+            return a + b;
+        } else if (opr.equals("Subtract")) {
+            return a - b;
+        } else if (opr.equals("Multiply")) {
+            return a * b;
+        } else if (opr.equals("Divide")) {
+            return a / b;
+        } else {
+            System.out.print("Invalid operation: ");
+            return 0;
+        }
     }
 }
 
-public class Problem1{
+public class Problem1 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+
         Calculator c = new Calculator();
-        System.out.print("Enter the value of a:");
-        c.a=scan.nextDouble();
-        System.out.print("Enter the value of b:");
-        c.b=scan.nextDouble();
-        System.out.println("Type the operation from below to perform(case  sensitive):\n Add\n Subtract\n Multiply\n Divide");
+
+        System.out.print("Enter the value of a: ");
+        c.a = scan.nextDouble();
+
+        System.out.print("Enter the value of b: ");
+        c.b = scan.nextDouble();
+
+        System.out.println("Type the operation from below to perform (case sensitive):");
+        System.out.println("Add\nSubtract\nMultiply\nDivide");
+
         c.opr = scan.next();
 
-        double result=c.calculate();
-        System.out.println("Result:"+result);
+        double result = c.calculate();
+        System.out.println("Result: " + result);
     }
 }
-
